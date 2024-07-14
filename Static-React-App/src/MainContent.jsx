1d8b5eb57dff3ca6dc5bf1796e28267330279b22
Havishya Vally Sunkara <83769088+havishya10@users.noreply.github.com>
2024-07-14 14:59:18 +0530

    Update MainContent.jsx

--- Static-React-App/src/MainContent.jsx
+++ Static-React-App/src/MainContent.jsx
@@ -6,7 +6,7 @@ export default function Main(props) {
       <h1>Fun facts about React</h1>
       <div>
         <ul>
-          <li>Was first released in 2013</li>
+          <li>Was first released in 2014</li>
           <li>Was originally created by Jordan Walke</li>
           <li>Has well over 100K stars on GitHub</li>
           <li>Is maintained by Facebook</li>
