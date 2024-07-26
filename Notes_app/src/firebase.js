daea327a475dbe8176a1bd82ad8d72a0bf3e03d9
Havishya Vally Sunkara <83769088+havishya10@users.noreply.github.com>
2024-07-26 17:30:10 +0530

    Update firebase.js with env variables

--- Notes_app/src/firebase.js
+++ Notes_app/src/firebase.js
@@ -1,37 +1,16 @@
-import "firebase/auth";
-import "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, onSnapshot } from "firebase/firestore";
-import "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQrXBVhHA_-Lz8QgKIxEHnjXTf3MgAepI",
-  authDomain: "react-notes-app-20ac4.firebaseapp.com",
-  projectId: "react-notes-app-20ac4",
-  storageBucket: "react-notes-app-20ac4.appspot.com",
-  messagingSenderId: "520793924542",
-  appId: "1:520793924542:web:f8f3b195181704be218fef",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const notesCollection = collection(db, "notes");
-
-// course firebase authentication
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore, collection } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD_uXh8KfNe11S8qhfQuB_RAzIHrKFTOHE",
-//   authDomain: "react-notes-7a013.firebaseapp.com",
-//   projectId: "react-notes-7a013",
-//   storageBucket: "react-notes-7a013.appspot.com",
-//   messagingSenderId: "370854322953",
-//   appId: "1:370854322953:web:3e20fb09a3b7f61fc329d8",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// export const notesCollection = collection(db, "notes");
